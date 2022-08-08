f568cfee7c220c36cd4c23e8588ee6340b4882ab
Faizan <faizaneissaf@gmail.com>
2022-08-08 12:14:33 -0700

    2nd Commit

--- app/src/main/java/com/example/itask_quizapplication/QuestionAnswers.java
+++ app/src/main/java/com/example/itask_quizapplication/QuestionAnswers.java
@@ -3,24 +3,49 @@ package com.example.itask_quizapplication;
 public class QuestionAnswers {
     public static String question[] = {
 
-            "'.MOV' extension usually refers to Animation/movie file file",
-            "In 1900s the first transatlantic radio broadcast occurs",
-            "Google Owns Android.",
-            "Notepad is a Programming Language",
-            "Elon musk is the owner of Microsoft"
-    };
-    public static  String choices[][]={
-            {"True","False"},
-            {"True","False"},
-            {"True","False"},
-            {"True","False"},
-            {"True","False"}
+            " The most common name in the world is Mohammed.",
+            "The bullet-proof vest, windscreen wipers, laser printers, and fire escapes were all invented by women.",
+            "Wearing headphones for just an hour will increase the bacteria in your ear by 700 times!",
+            "Like fingerprints, everyone’s tongue print is also unique.",
+            "The longest muscle in the body is the tongue.",
+            " More than half of our bones are located in our hands, wrists, ankles, and feet.",
+            " Human thigh bones are stronger than concrete.",
+            "People say, “Bless You,” when you sneeze because your heart stops for a millisecond.",
+            "Japan is also called the land of the rising sun.",
+            "Elephants are the only animals that can’t jump.",
+            "Dragonflies are one of the fastest insects and fly at 50 to 60 mph.",
+            " The blood of mammals is red, the blood of insects is yellow, and the blood of lobster is blue.",
+            "A cockroach can live several weeks with its head cut off.",
+            "The hummingbird, the loon, the swift, the kingfisher, and the greb are all birds that cannot walk.",
+            "The fastest bird is the Peregrine falcon, clocked at speeds of up to 240 miles per hour.",
+            "The electric chair was invented by a dentist.",
+            "Corn is grown on every continent except antarctica.",
+            "A cat's tail contains nearly 10 percent of all the bones in its body.",
+            "Hearing is the fastest human sense. a person can recognize a sound in as little as 0.05 seconds.",
+            "Rhythm'' is the longest English word without a vowel.",
+            "Earth is the only planet not named after a god."
     };
     public static String correctAnswers[]={
             "True",
             "True",
             "True",
-            "False",
-            "False"
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True",
+            "True"
     };
 }
--- app/src/main/java/com/example/itask_quizapplication/Quiz_Page.java
+++ app/src/main/java/com/example/itask_quizapplication/Quiz_Page.java
@@ -2,33 +2,37 @@ package com.example.itask_quizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Quiz_Page extends AppCompatActivity implements View.OnClickListener{
 
     TextView totalQtv;
-    TextView question_tv;
+    TextView question_tv,qnum;
     Button ansT,ansF;
 
+    Dialog dialog;
     int score=0;
     int totalQuestion = QuestionAnswers.question.length;
     int currentQuestionIndex=0;
-    String selectedAnswer="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_page);
-        totalQtv=findViewById(R.id.tv_tq);
+        totalQtv=findViewById(R.id.tv_tquestions);
         question_tv=findViewById(R.id.tv_q);
+        qnum=findViewById(R.id.q_num);
         ansT=findViewById(R.id.btn_true);
         ansF=findViewById(R.id.btn_false);
+
+        dialog=new Dialog(this);
 
         ansF.setOnClickListener(this);
         ansT.setOnClickListener(this);
@@ -36,57 +40,56 @@ public class Quiz_Page extends AppCompatActivity implements View.OnClickListener
         loadNewQuestion();
     }
 
-
-    @Override
-    public void onClick(View view) {
-        Button clickedButton = (Button) view;
-        if (clickedButton.getId()==R.id.btn_true){
-            if (selectedAnswer.equals("True")){
-                score++;
-            }
-            selectedAnswer = clickedButton.getText().toString();
-            currentQuestionIndex++;
-            loadNewQuestion();
-
-        }else{
-            selectedAnswer = clickedButton.getText().toString();
-            currentQuestionIndex++;
-            loadNewQuestion();
-        }
-    }
-    void loadNewQuestion(){
-        question_tv.setText(QuestionAnswers.question[currentQuestionIndex]);
-        ansT.setText(QuestionAnswers.choices[currentQuestionIndex][0]);
-        ansF.setText(QuestionAnswers.choices[currentQuestionIndex][1]);
+    private void loadNewQuestion() {
         if (currentQuestionIndex==totalQuestion){
             finishQuiz();
             return;
         }
+        question_tv.setText(QuestionAnswers.question[currentQuestionIndex]);
+        qnum.setText(currentQuestionIndex+"");
     }
-    void finishQuiz(){
-        String passStatus="";
-        if (score>totalQuestion*0.60){
-            passStatus="Passed";
-        }else {
-            passStatus="Failed";
-        }
-        new AlertDialog.Builder(this)
-                .setTitle(passStatus)
-                .setMessage("Score is "+score+" out of "+totalQuestion)
-                .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        restartQuiz();
-                    }
-                })
-                .setCancelable(false)
-                .show();
-        restartQuiz();
 
+    private void finishQuiz() {
+        dialog.setContentView(R.layout.result_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button rbtn=dialog.findViewById(R.id.restart_btn);
+        Button cont=dialog.findViewById(R.id.btn_contribution);
+        TextView rst=dialog.findViewById(R.id.r_tv);
+        rst.setText(score+" / "+totalQuestion);
+        cont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),DonateActivity.class);
+                startActivity(intent);
+                dialog.setCancelable(false);
+            }
+        });
+        rbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score=0;
+                currentQuestionIndex=0;
+                loadNewQuestion();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        dialog.setCancelable(false);
     }
-    void restartQuiz(){
-        score=0;
-        currentQuestionIndex=0;
+
+    @Override
+    public void onClick(View view) {
+        Button clickedButton = (Button) view;
+        String ans=clickedButton.getText().toString();
+        String cans=QuestionAnswers.correctAnswers[currentQuestionIndex].toString();
+        if(ans.equals(cans)){
+            score++;
+            currentQuestionIndex++;
+        }
+        else {
+            currentQuestionIndex++;
+        }
         loadNewQuestion();
     }
 }
